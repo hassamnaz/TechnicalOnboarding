dd7e32a5f2554f2fc5dbaf7815402adfc85ffca3
Hassam Naz <125550490+hassamnaz@users.noreply.github.com>
2023-06-17 16:59:32 +0500

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,33 +10,33 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/blackbg.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+const description = "MLSA-Alpha | GDSC | Graphic & UI/UX Designer | Game UI Designer | C++";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "UI/UX Designer",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Web design",
+  "Mobile App Design",
   "Mobile user interfaces",
   "Graphic design",
+  "Game UI Design",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Experienced Game UI Designer with Graphic & UI/UX Design background. Proficient in industry-standard design tools and techniques, with 2 years of experience in designing and developing game UIs, web and mobile apps, and other digital products. Member of GDSC Design Team and Microsoft Learn Student Ambassadors. Passionate about creating immersive user experiences and staying up-to-date with industry trends. Looking to connect with fellow designers and game enthusiasts to explore new opportunities.";
 
 const About = () => {
   return (
